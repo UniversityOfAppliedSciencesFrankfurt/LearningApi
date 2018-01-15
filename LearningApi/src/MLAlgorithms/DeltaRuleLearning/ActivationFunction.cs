ce8d5758b9d9728632ab6d2d4141cfb1a71eeecc
Software Engineering WS2014 <swe-guardians@outlook.com>
2018-01-15 01:18:21 +0100

    added DeltaRuleLearning to ML Algorithms

---
+++ LearningApi/src/MLAlgorithms/DeltaRuleLearning/ActivationFunction.cs
@@ -0,0 +1,21 @@
+﻿using LearningFoundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeltaLearning
+{
+    public class ActivationFunction
+    {
+        public static double Boolean(double val)
+        {
+            return (val > 1) ? 1 : 0;
+        }
+
+        public static double Sigmoid(double val)
+        {
+            return 1 / (1 + Math.Exp(-val));
+        }
+    }
+}
