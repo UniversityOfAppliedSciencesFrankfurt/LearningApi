18b9f140242774781783ff49bad954bf14f14b8b
Sven E <sven-eisenbach3@hotmail.de>
2019-03-07 15:37:12 +0100

    Migrated EuclideanColorFilter to LearingAPI

---
+++ LearningApi/EuclideanColorFilter/EuclideanFilter/CalcDistance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace EuclideanFilter
+{
+    public class CalcDistance
+    {
+        /// <summary>
+        /// Calculate the Distance between two Points in 3D with the formula: distance = Sqrt((x2 - x1)² + (y2 - y1)² + (z2 - z1)²)    
+        /// </summary>
+        /// <param name="pixelColor"></param>
+        /// <param name="center"></param>
+        /// <returns></returns>
+        public static float ComputeEuclideanDistance(Color pixelColor, Color center)
+        {
+            float r2 = (float) Math.Pow(pixelColor.R - center.R, 2);
+            float g2 = (float) Math.Pow(pixelColor.G - center.G, 2);
+            float b2 = (float) Math.Pow(pixelColor.B - center.B, 2);
+
+            return (float) Math.Sqrt(r2 + g2 + b2);
+        }
+    }
+}
