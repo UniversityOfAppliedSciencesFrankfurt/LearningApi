93afebebd1f1e9a30441159195c30cb57705a932
PatrickRenner <patrick.renner@daenet.eu>
2019-11-06 16:45:00 +0100

    Added SupportVectorMachineAlgorithm project

---
+++ LearningApi/src/MLAlgorithms/SupportVectorMachineAlgorithm/SVMResult.cs
@@ -0,0 +1,19 @@
+﻿using LearningFoundation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningFoundation.MlAlgorithms.SupportVectorMachineAlgorithm
+{
+    /// <summary>
+    /// SVMResult is a class that contains the response object of the SVM's Predict function.
+    /// </summary>
+    public class SVMResult : IResult
+    {
+        /// <summary>
+        /// List of results. 1 and -1 are different value of two classes. 
+        /// </summary>
+        public int[] PredictedResult { get; set; }
+        
+    }
+}
