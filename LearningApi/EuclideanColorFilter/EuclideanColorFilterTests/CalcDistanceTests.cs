247e13ce12cbb87474feb8e14480fed7030f96d4
dbetkar2 <57078001+dbetkar2@users.noreply.github.com>
2020-03-07 01:31:25 +0100

    Add Licensed header to unit test

--- LearningApi/EuclideanColorFilter/EuclideanColorFilterTests/CalcDistanceTests.cs
+++ LearningApi/EuclideanColorFilter/EuclideanColorFilterTests/CalcDistanceTests.cs
@@ -1,3 +1,5 @@
+// Copyright (c) daenet GmbH / Frankfurt University of Applied Sciences. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
--- LearningApi/EuclideanColorFilter/EuclideanColorFilterTests/EuclideanFilterModuleTester.cs
+++ LearningApi/EuclideanColorFilter/EuclideanColorFilterTests/EuclideanFilterModuleTester.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿// Copyright (c) daenet GmbH / Frankfurt University of Applied Sciences. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Drawing;
 using EuclideanFilter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LearningFoundation;
