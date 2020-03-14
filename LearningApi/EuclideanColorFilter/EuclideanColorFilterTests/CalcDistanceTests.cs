af94907db8af0f01bd0ec15bf3d14c9181d0f3e3
dbetkar2 <57078001+dbetkar2@users.noreply.github.com>
2020-03-14 16:27:21 +0100

    change reference file name LearningFoundation.namespace

--- LearningApi/EuclideanColorFilter/EuclideanColorFilterTests/CalcDistanceTests.cs
+++ LearningApi/EuclideanColorFilter/EuclideanColorFilterTests/CalcDistanceTests.cs
@@ -3,7 +3,7 @@
 
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using EuclideanColorFilter;
+using LearningFoundation.EuclideanColorFilter;
 using System.Drawing;
 
 namespace LearningFoundation.EuclideanColorFilterTests
